2c7b3244f225d8f12449bb7d04608a39cf0f2d3d
Gabriel <gabriel99luques@gmail.com>
2022-06-17 17:52:25 -0300

    Properties to represent MutableLiveData and LiveData for a single amphibian object

--- app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -28,11 +28,15 @@ class AmphibianViewModel : ViewModel() {
     // TODO: Create properties to represent MutableLiveData and LiveData for the API status
     private val _status: MutableLiveData<AmphibianApiStatus> = MutableLiveData()
     val status : LiveData<AmphibianApiStatus> = _status
+
     // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
     private val _amphibians : MutableLiveData<List<Amphibian>> = MutableLiveData()
-    val amphibian : LiveData<List<Amphibian>> = _amphibians
+    val amphibians : LiveData<List<Amphibian>> = _amphibians
+
     // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
     //  This will be used to display the details of an amphibian when a list item is clicked
+    private val _amphibian : MutableLiveData<Amphibian> = MutableLiveData()
+    val amphibian : LiveData<Amphibian> = _amphibian
 
     // TODO: Create a function that gets a list of amphibians from the api service and sets the
     //  status via a Coroutine
